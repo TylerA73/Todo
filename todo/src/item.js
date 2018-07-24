d207c825e65987624a92e58d9398765c60047a9a
arseneaultt2 <arseneaultt2@mymacewan.ca>
2018-07-24 13:41:59 -0600

    Set up as npm

--- todo/src/item.js
+++ todo/src/item.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Item extends Component {
 	render(){
+		// return a new list item using the desc prop as the content
 		return(
 			<li>
-				Item {this.props.desc}
+				{this.props.desc}
 			</li>
 		);
 	}
